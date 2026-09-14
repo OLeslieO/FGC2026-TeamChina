594d2e8b0ea80d7231e4d8f120b2084f6e0a777c
OLeslieO <leslietan0521@gmail.com>
2026-09-14 23:47:04 +0800

    Update Constants.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Constants.java
@@ -5,9 +5,9 @@ public enum Constants {
     DRIVE_SLOW_MULTIPLIER(0.3),
     SHOOTER_SHOOT_POW(1.0),
     SHOOTER_IDLE_POW(0.3),
-    TRANSFER_POW(1.0),
+    TRANSFER_POW(0.8),
     INTAKE_PWR(1.0),
-    LALA_PWR(1.0),
+    LALA_PWR(1.0);
     public final double value;
     Constants(double value) {
         this.value = value;
